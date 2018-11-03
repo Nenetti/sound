8475f20195c2fcef2af3c9baf8b2a1a8eee6a3b3
Nenetti <akinori1090@gmail.com>
2018-11-04 02:49:47 +0900

    0.1

--- voice/src/main/java/ros/ServiceClient.java
+++ voice/src/main/java/ros/ServiceClient.java
@@ -3,42 +3,63 @@ package ros;
 import org.ros.message.MessageListener;
 import org.ros.node.ConnectedNode;
 
+import ros.MessageType.Type;
+
 
 public class ServiceClient {
 	
 	private Subscriber subscriber;
 	private Publisher publisher;
+	private Object result;
 	private boolean isResponse=false;
 	private boolean isError=false;
+	private Type type;
 	
 	public ServiceClient(ConnectedNode connectedNode, String topic, String type) {
+		constructor(connectedNode, topic, type, std_msgs.Int32._TYPE);
+	}
+	
+	public ServiceClient(ConnectedNode connectedNode, String topic, String publish_type, String subscribe_type) {
+		constructor(connectedNode, topic, publish_type, subscribe_type);
+	}
+	
+	public void constructor(ConnectedNode connectedNode, String topic, String publish_type, String subscribe_type) {
 		String server_topic=topic+"_"+"server";
 		String client_topic=topic+"_"+"client";
-		this.subscriber=new Subscriber(connectedNode, client_topic, std_msgs.Int32._TYPE);
-		this.publisher=new Publisher(connectedNode, server_topic, type);
+		this.type=Type.getType(subscribe_type);
+		this.subscriber=new Subscriber(connectedNode, client_topic, subscribe_type);
+		this.publisher=new Publisher(connectedNode, server_topic, publish_type);
 		this.subscriber.addMessageListener(new MessageListener<Object>() {
 			@Override
 			public void onNewMessage(Object message) {
+				switch (type) {
+				case Int32:
+					result=((std_msgs.Int32)message).getData();
+					break;
+				case String:
+					result=((std_msgs.String)message).getData();
+					break;
+				}
 				isResponse=true;
-				/*if(message.getData()==0) {
-					isError=false;
-				}else {
-					isError=true;
-				}*/
 			}
 		});
 	}
 	
 	public ServiceClient publish(Object data) {
+		isResponse=false;
 		publisher.publish(data);
 		return this;
 	}
 	
-	public void waitForServer() {
+	public Object waitForServer() {
 		while(!isResponse) {
 			NodeHandle.duration(1);
 		}
-		isResponse=false;
+		return getResult();
+	}
+	
+	public Object getResult() {
+		return result;
 	}
 	
 	public boolean isError() {
